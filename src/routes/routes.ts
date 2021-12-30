9b3e7d97b79e1c9d7c732f77050d71005ee13364
Carlos Alves <carlosalves369m@hotmail.com>
2021-12-30 10:56:18 -0300

    :hammer: Config socket

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,5 +1,6 @@
 const router = require('express').Router()
 import { Request, Response } from 'express'
+import path from 'path'
 
 router.get('/author', (req: Request, res: Response) => {
     return res.json({ 
@@ -8,6 +9,10 @@ router.get('/author', (req: Request, res: Response) => {
         repo_link:  "https://github.com/EuCarlos/chat",
         website: "https://carlosalves.now.sh"
     })
+})
+
+router.get('/', (req: Request, res: Response) => {
+    res.sendFile(path.join(__dirname, '../../public/view/index.html'))
 })
 
 export = router
--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,20 @@ import express from 'express'
 import http from 'http'
 import routes from './routes/routes'
 
+
 const app = express()
 const server = new http.Server(app)
+var io = require('socket.io')(server)
 const port = process.env.PORT || 8080
 
 app.use('/', routes)
 
 server.listen(port, () => console.log(`🔥 Server is running in PORT ${port}`))
+
+io.on('connection', (socket: any) => {
+    console.log('A user has connected')
+
+    socket.on('disconnect', () => {
+        console.log('A user has disconnected')
+    })
+})
